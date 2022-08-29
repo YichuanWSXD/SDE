c1c7db8c27117b806e3fe4b32fdc0d0f622003c2
Yichuan <wsxdwang@gmail.com>
2022-08-28 19:04:05 -0500

    Try Redis with Spring Boot
    Try Redis with Spring Boot

---
+++ Java/SpringBoot/springboot-redis/src/test/java/com/ityichuan/springbootredis/SpringbootRedisApplicationTests.java
@@ -0,0 +1,30 @@
+package com.ityichuan.springbootredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@SpringBootTest
+class SpringbootRedisApplicationTests {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Test
+    public void testSet() {
+        redisTemplate.boundValueOps("name").set("John");
+    }
+
+    @Test
+    public void testGet() {
+        Object name = redisTemplate.boundValueOps("name").get();
+        System.out.println(name);
+    }
+
+
+    @Test
+    void contextLoads() {
+    }
+
+}
